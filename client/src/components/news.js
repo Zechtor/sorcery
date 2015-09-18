d176367a5134c5c1d4cafa00fbe53b6cc3df890b
Trent Carden <trentcarden@gmail.com>
2015-09-18 19:08:02 -0400

    Initial refresh work

--- client/src/components/news.js
+++ client/src/components/news.js
@@ -9,30 +9,50 @@ var News = React.createClass({
 
     getInitialState: function() {
         return {
-            news: NewsService.news
+            news: NewsService.news,
+            isLoading: false
         };
     },
 
     componentDidMount: function() {
         // initial data load
-        var that = this; // TODO: remove this dependency
+        var self = this; // TODO: remove this dependency
         NewsService.get(function() {
-            that.setState({
-                news: NewsService.news
+            self.setState({
+                news: NewsService.news,
+                isLoading: true
             });
+            console.log('pants', self.state);
+        });
+    },
+
+    refresh: function(event) {
+        this.setState({isLoading: true});
+        console.log('holy shit', this.state.isLoading);
+        var self = this; // TODO: remove this dependency
+        NewsService.get(function() {
+            self.setState({
+                news: NewsService.news,
+                isLoading: false
+            });
+            console.log('holy crap', self.state.isLoading);
         });
     },
     
     render : function() {
         return (
             <section className="news">
-                <Header title="News" />
+                <Header title="News"/> 
                 <Container>
                     <List>
+                    <input type="button" onClick={this.refresh} value="Refresh" />
                         {this.state.news.map(function(article){ 
                             return <NewsArticle data={article} />;
                         })}
                     </List>
+                        { this.state.isLoading &&
+                            <Loader />
+                        }
                 </Container>
             </section>
         );
@@ -57,6 +77,16 @@ var NewsArticle = React.createClass({
                 </div>
             </li>
         );
+    }
+});
+
+var Loader = React.createClass({
+    render : function() {
+        return (
+            <div className ="Loader">
+            LOADING MAGIC
+            </div>
+            );
     }
 });
 
