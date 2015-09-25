3a83d134eca5ce3f98a36f99f08a433657a09e77
Trent Carden <trentcarden@gmail.com>
2015-09-25 19:52:56 -0400

    minor fixes

--- server/models/article.py
+++ server/models/article.py
@@ -8,7 +8,7 @@ class Article(Base):
     createDate = Column(DateTime, nullable=False)
     postDate = Column(DateTime, nullable=False)
     title = Column(String(200), nullable=False)
-    articleUrl = Column(String(200))
+    articleUrl = Column(String(200), nullabe=False)
     imageUrl = Column(String(200))
 
     @classmethod
