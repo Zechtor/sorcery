3ce4c9c7260d6d719cc2ba0a575213db5c2e7484
zechtor <bryan.dilks@gmail.com>
2015-09-26 14:21:48 -0700

    responsive-mobile

--- client/src/components/news.jsx
+++ client/src/components/news.jsx
@@ -55,9 +55,9 @@ var News = React.createClass({
     
     render : function() {
         return (
-            <section className="news">
+            <section id="news">
                 <Header title="News">                
-                    <input type="button" onClick={this.refresh} value="Refresh" />
+                    <button className="refresh" onClick={this.refresh}>Refresh</button>
                 </Header>
                 <Container>
                     <List>
