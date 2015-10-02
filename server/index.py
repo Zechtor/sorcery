c746b21bb0841a05c0ba586e6efa3463def8a682
zechtor <bryan.dilks@gmail.com>
2015-10-01 18:39:40 -0700

    indexer-commands

--- server/index.py
+++ server/index.py
@@ -1,17 +1,35 @@
-import sched, time
-from indexers.tweet import TweetIndexer
+import sched, sys, time
+
 from indexers.news import NewsIndexer
 from indexers.team import TeamIndexer
+from indexers.tweet import TweetIndexer
 from indexers.schedule import ScheduleIndexer
 
-s = sched.scheduler(time.time, time.sleep)
-def runIndexers(sc): 
-    # execute all index functions here
-    #TeamIndexer().index()
-    #TweetIndexer().index()
-    #NewsIndexer().index()
-    #ScheduleIndexer().index()
-    sc.enter(60*15, 1, runIndexers, (sc,))
+def index(args):
 
-runIndexers(s)
-#s.run()
+	if len(args) == 2 and args[1] == 'setup':
+		runOnce()
+		return
+
+	ts = sched.scheduler(time.time, time.sleep)
+	scheduleTweets(ts)
+	ts.run()
+
+	#ns = sched.scheduler(time.time, time.sleep)
+	#scheduleNews(ns)
+	#ns.run()
+
+def runOnce(): 
+    # indexers that only needed to be run rarely
+    TeamIndexer().index()
+    ScheduleIndexer().index()
+
+def scheduleTweets(sc): 
+    TweetIndexer().index()
+    sc.enter(60 * 15, 1, scheduleTweets, (sc,))
+
+def scheduleNews(sc): 
+    NewsIndexer().index()
+    sc.enter(12 * 60 * 15, 1, scheduleNews, (sc,))
+
+index(sys.argv)
